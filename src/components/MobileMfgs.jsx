a450961aebac4e639ac8bfdf043b5105924bf6fd
Sagar-Gondage <sagargondage370@gmail.com>
2022-05-24 08:24:47 +0530

    second commit

---
+++ src/components/MobileMfgs.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MobileMfgs = () => {
+  return (
+    <div>
+        <h2>Mobile Manufactureres</h2>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+  )
+}
+
+export default MobileMfgs
